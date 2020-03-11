06fb12e5795a97c1f2d06920f984127aa987d3c2
Burke Shartsis <burke@differential.com>
2020-03-11 00:53:32 -0400

    working android setup

--- apolloschurchapp/android/app/src/main/java/com/apolloschurchapp/MainActivity.java
+++ apolloschurchapp/android/app/src/main/java/com/apolloschurchapp/MainActivity.java
@@ -4,14 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import org.devio.rn.splashscreen.SplashScreen;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
-        super.onCreate(savedInstanceState);
-    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -19,6 +14,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "apolloschurchapp";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RNBootSplash.init(R.drawable.bootsplash, com.apolloschurchapp.MainActivity.this);
     }
 
     @Override
