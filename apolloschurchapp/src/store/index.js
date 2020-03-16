12819ad99294c9272a18920174e074071d70b53f
Burke Shartsis <burke@differential.com>
2020-03-16 12:40:43 -0400

    try to fix hanging tests?
    Co-Authored-By: Vincent Wilson <vinnyjth@users.noreply.github.com>

--- apolloschurchapp/src/store/index.js
+++ apolloschurchapp/src/store/index.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 import { schema as mediaPlayerSchema } from '@apollosproject/ui-media-player';
 import { updatePushId } from '@apollosproject/ui-notifications';
-import { CACHE_LOADED } from '../client/cache'; // eslint-disable-line
+import CACHE_LOADED from '../client/getCacheLoaded'; // eslint-disable-line
 
 // TODO: this will require more organization...ie...not keeping everything in one file.
 // But this is simple while our needs our small.
