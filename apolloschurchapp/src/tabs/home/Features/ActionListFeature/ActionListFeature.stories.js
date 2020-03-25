88a60b6a6dca63fa65ccd193341c6af80fabebed
Burke Shartsis <burke@differential.com>
2020-03-25 13:41:43 -0400

    update and fix stories for ActionListFeature

--- apolloschurchapp/src/tabs/home/Features/ActionListFeature/ActionListFeature.stories.js
+++ apolloschurchapp/src/tabs/home/Features/ActionListFeature/ActionListFeature.stories.js
@@ -1,8 +1,152 @@
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
 
+import { BackgroundView, CenteredView } from '@apollosproject/ui-kit';
 import ActionListFeature from '.';
 
-storiesOf('ActionListFeature', module).add('default', () => (
-  <ActionListFeature />
-));
+storiesOf('ActionListFeature', module)
+  .addDecorator((story) => (
+    <BackgroundView>
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <CenteredView style={{ alignItems: 'stretch' }}>{story()}</CenteredView>
+    </BackgroundView>
+  ))
+  .add('default', () => {
+    const actions = [
+      {
+        id: 'fakeId1',
+        title: 'Boom',
+        subtitle: 'What',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: 'https://picsum.photos/200/200',
+            },
+          ],
+        },
+      },
+      {
+        id: 'fakeId2',
+        title: 'Boom',
+        subtitle: 'What',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: 'https://picsum.photos/200',
+            },
+          ],
+        },
+      },
+      {
+        id: 'fakeId3',
+        title: 'Boom',
+        subtitle: 'What',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: 'https://picsum.photos/200',
+            },
+          ],
+        },
+      },
+      {
+        id: 'fakeId4',
+        title: 'Boom',
+        subtitle: 'What',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: 'https://picsum.photos/200',
+            },
+          ],
+        },
+      },
+    ];
+    return <ActionListFeature actions={actions} />;
+  })
+  .add('isLoading', () => {
+    const loadingStateData = [
+      {
+        id: 'fakeId1',
+        title: '',
+        subtitle: '',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: '',
+            },
+          ],
+        },
+      },
+      {
+        id: 'fakeId2',
+        title: '',
+        subtitle: '',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: '',
+            },
+          ],
+        },
+      },
+      {
+        id: 'fakeId3',
+        title: '',
+        subtitle: '',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: '',
+            },
+          ],
+        },
+      },
+      {
+        id: 'fakeId4',
+        title: '',
+        subtitle: '',
+        parentChannel: {
+          id: '',
+          name: '',
+        },
+        image: {
+          sources: [
+            {
+              uri: '',
+            },
+          ],
+        },
+      },
+    ];
+
+    return <ActionListFeature actions={loadingStateData} isLoading />;
+  });
