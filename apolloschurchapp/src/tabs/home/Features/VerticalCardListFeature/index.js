46297578b120e44e85c8106e08b7f31685ed889c
Burke Shartsis <burke@differential.com>
2020-03-26 14:18:27 -0400

    basic working VerticalCardListFeature component
    includes commented code to handle a single loading state if we want to 
    use that later

--- apolloschurchapp/src/tabs/home/Features/VerticalCardListFeature/index.js
+++ apolloschurchapp/src/tabs/home/Features/VerticalCardListFeature/index.js
@@ -3,38 +3,65 @@ import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 
-import { FeedView, H3, H6, PaddedView, styled } from '@apollosproject/ui-kit';
+import {
+  FeedView,
+  H3,
+  H6,
+  PaddedView,
+  styled,
+  withIsLoading,
+} from '@apollosproject/ui-kit';
 import { contentCardComponentMapper } from '@apollosproject/ui-connected';
 
 const StyledH6 = styled(({ theme }) => ({
   color: theme.colors.text.tertiary,
 }))(H6);
 
-const VerticalCardListFeature = ({
-  cards,
-  isLoading,
-  onPressItem,
-  subtitle,
-  title,
-}) => (
-  <View>
-    <PaddedView>
-      {isLoading || title ? ( // we check for isloading here so that they are included in the loading state
-        <StyledH6 numberOfLines={1}>{title}</StyledH6>
-      ) : null}
-      {isLoading || subtitle ? <H3>{subtitle}</H3> : null}
-    </PaddedView>
-    <FeedView
-      onPressItem={onPressItem}
-      ListItemComponent={contentCardComponentMapper}
-      content={cards.map((card) => ({
-        ...card,
-        coverImage: get(card, 'coverImage.sources', undefined),
-        __typename: card.relatedNode.__typename,
-      }))}
-      isLoading={isLoading}
-    />
-  </View>
+// const getContent = ({ cards, isLoading }) => {
+//   let content = [];
+//   if (isLoading && !cards.length) {
+//     content = [
+//       {
+//         id: 'fakeId0',
+//         isLoading: true,
+//         title: 'Test',
+//         summary: 'Boom',
+//         channelType: '',
+//         coverImage: [],
+//         parentChannel: {
+//           id: '',
+//           name: '',
+//         },
+//       },
+//     ];
+//   } else {
+//     content = cards.map((card) => ({
+//       ...card,
+//       coverImage: get(card, 'coverImage.sources', undefined),
+//       __typename: card.relatedNode.__typename,
+//     }));
+//   }
+//
+//   return content;
+// };
+
+const VerticalCardListFeature = memo(
+  ({ cards, isLoading, onPressItem, subtitle, title }) => (
+    <View>
+      <PaddedView>
+        {isLoading || title ? ( // we check for isloading here so that they are included in the loading state
+          <StyledH6 numberOfLines={1}>{title}</StyledH6>
+        ) : null}
+        {isLoading || subtitle ? <H3>{subtitle}</H3> : null}
+      </PaddedView>
+      <FeedView
+        onPressItem={onPressItem}
+        ListItemComponent={contentCardComponentMapper}
+        content={cards} // {getContent({ cards, isLoading })}
+        isLoading={isLoading}
+      />
+    </View>
+  )
 );
 
 VerticalCardListFeature.displayName = 'Features';
@@ -47,4 +74,4 @@ VerticalCardListFeature.propTypes = {
   title: PropTypes.string,
 };
 
-export default VerticalCardListFeature;
+export default withIsLoading(VerticalCardListFeature);
