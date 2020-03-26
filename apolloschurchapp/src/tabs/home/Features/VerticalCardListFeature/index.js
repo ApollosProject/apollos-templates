77d380e3949b19ad72cf719ef0a1f98176c801b0
Burke Shartsis <burke@differential.com>
2020-03-26 02:02:46 -0400

    add title and subtitle field. clean up props

--- apolloschurchapp/src/tabs/home/Features/VerticalCardListFeature/index.js
+++ apolloschurchapp/src/tabs/home/Features/VerticalCardListFeature/index.js
@@ -1,25 +1,40 @@
 import React, { memo } from 'react';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 
-import { FeedView, H3, H6, styled } from '@apollosproject/ui-kit';
+import { FeedView, H3, H6, PaddedView, styled } from '@apollosproject/ui-kit';
 import { contentCardComponentMapper } from '@apollosproject/ui-connected';
 
 const StyledH6 = styled(({ theme }) => ({
   color: theme.colors.text.tertiary,
 }))(H6);
 
-const VerticalCardListFeature = ({ cards, isLoading, onPressItem }) => (
-  <FeedView
-    onPressItem={onPressItem}
-    ListItemComponent={contentCardComponentMapper}
-    content={cards.map((card) => ({
-      ...card,
-      coverImage: get(card, 'coverImage.sources', undefined),
-      __typename: card.relatedNode.__typename,
-    }))}
-    isLoading={isLoading}
-  />
+const VerticalCardListFeature = ({
+  cards,
+  isLoading,
+  onPressItem,
+  subtitle,
+  title,
+}) => (
+  <View>
+    <PaddedView>
+      {isLoading || title ? ( // we check for isloading here so that they are included in the loading state
+        <StyledH6 numberOfLines={1}>{title}</StyledH6>
+      ) : null}
+      {isLoading || subtitle ? <H3>{subtitle}</H3> : null}
+    </PaddedView>
+    <FeedView
+      onPressItem={onPressItem}
+      ListItemComponent={contentCardComponentMapper}
+      content={cards.map((card) => ({
+        ...card,
+        coverImage: get(card, 'coverImage.sources', undefined),
+        __typename: card.relatedNode.__typename,
+      }))}
+      isLoading={isLoading}
+    />
+  </View>
 );
 
 VerticalCardListFeature.displayName = 'Features';
