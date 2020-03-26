53019116f4aaa494397456f1ba8854095fae98cf
Burke Shartsis <burke@differential.com>
2020-03-25 22:54:23 -0400

    default test for ActionListCardFeature

---
+++ apolloschurchapp/src/tabs/home/Features/ActionListCardFeature/ActionListCardFeature.tests.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Providers from '../../../../Providers';
+
+import ActionListCardFeature from '.';
+
+const actions = [
+  {
+    id: 'fakeId1',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200/200',
+        },
+      ],
+    },
+  },
+  {
+    id: 'fakeId2',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200',
+        },
+      ],
+    },
+  },
+  {
+    id: 'fakeId3',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200',
+        },
+      ],
+    },
+  },
+  {
+    id: 'fakeId4',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200',
+        },
+      ],
+    },
+  },
+];
+
+describe('The Onboarding LandingScreen component', () => {
+  it('should render', () => {
+    const tree = renderer.create(
+      <Providers>
+        <ActionListCardFeature actions={actions} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
