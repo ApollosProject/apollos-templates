9837662da4c67034448a72a877feb270b2606f41
Burke Shartsis <burke@differential.com>
2020-03-25 22:41:49 -0400

    complete story coverage for ActionListCardFeature

--- apolloschurchapp/src/tabs/home/Features/ActionListCardFeature/ActionListCardFeature.stories.js
+++ apolloschurchapp/src/tabs/home/Features/ActionListCardFeature/ActionListCardFeature.stories.js
@@ -4,6 +4,73 @@ import { storiesOf } from '@apollosproject/ui-storybook';
 import { BackgroundView, CenteredView } from '@apollosproject/ui-kit';
 import ActionListFeature from '.';
 
+const actions = [
+  {
+    id: 'fakeId1',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200/200',
+        },
+      ],
+    },
+  },
+  {
+    id: 'fakeId2',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200',
+        },
+      ],
+    },
+  },
+  {
+    id: 'fakeId3',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200',
+        },
+      ],
+    },
+  },
+  {
+    id: 'fakeId4',
+    title: 'Boom',
+    subtitle: 'What',
+    parentChannel: {
+      id: '',
+      name: '',
+    },
+    image: {
+      sources: [
+        {
+          uri: 'https://picsum.photos/200',
+        },
+      ],
+    },
+  },
+];
+
 storiesOf('ActionListCardFeature', module)
   .addDecorator((story) => (
     <BackgroundView>
@@ -11,75 +78,15 @@ storiesOf('ActionListCardFeature', module)
       <CenteredView style={{ alignItems: 'stretch' }}>{story()}</CenteredView>
     </BackgroundView>
   ))
-  .add('default', () => {
-    const actions = [
-      {
-        id: 'fakeId1',
-        title: 'Boom',
-        subtitle: 'What',
-        parentChannel: {
-          id: '',
-          name: '',
-        },
-        image: {
-          sources: [
-            {
-              uri: 'https://picsum.photos/200/200',
-            },
-          ],
-        },
-      },
-      {
-        id: 'fakeId2',
-        title: 'Boom',
-        subtitle: 'What',
-        parentChannel: {
-          id: '',
-          name: '',
-        },
-        image: {
-          sources: [
-            {
-              uri: 'https://picsum.photos/200',
-            },
-          ],
-        },
-      },
-      {
-        id: 'fakeId3',
-        title: 'Boom',
-        subtitle: 'What',
-        parentChannel: {
-          id: '',
-          name: '',
-        },
-        image: {
-          sources: [
-            {
-              uri: 'https://picsum.photos/200',
-            },
-          ],
-        },
-      },
-      {
-        id: 'fakeId4',
-        title: 'Boom',
-        subtitle: 'What',
-        parentChannel: {
-          id: '',
-          name: '',
-        },
-        image: {
-          sources: [
-            {
-              uri: 'https://picsum.photos/200',
-            },
-          ],
-        },
-      },
-    ];
-    return <ActionListFeature actions={actions} />;
-  })
+  .add('example', () => (
+    <ActionListFeature
+      actions={actions}
+      title={'Title'}
+      subtitle={'Subtitle'}
+      onPressActionListButton={() => {}}
+    />
+  ))
+  .add('default', () => <ActionListFeature actions={actions} />)
   .add('isLoading', () => {
     const loadingStateData = [
       {
@@ -149,4 +156,11 @@ storiesOf('ActionListCardFeature', module)
     ];
 
     return <ActionListFeature actions={loadingStateData} isLoading />;
-  });
+  })
+  .add('onPressActionListButton', () => (
+    <ActionListFeature actions={actions} onPressActionListButton={() => {}} />
+  ))
+  .add('subtitle', () => (
+    <ActionListFeature actions={actions} subtitle={'subtitle'} />
+  ))
+  .add('title', () => <ActionListFeature actions={actions} title={'Title'} />);
