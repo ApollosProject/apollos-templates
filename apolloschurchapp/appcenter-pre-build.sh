d998f4123f633507015d3efac885423beaf4c5ef
Vincent Wilson <vince@classyh.at>
2020-03-30 11:17:36 -0400

    Debug

--- apolloschurchapp/appcenter-pre-build.sh
+++ apolloschurchapp/appcenter-pre-build.sh
@@ -6,8 +6,12 @@ cp $droid_dir/gradle.properties.appcenter $droid_dir/gradle.properties
 
 cd ios
 pod install
+cd ..
 
 # You can comment this out if your app doesn't have an existing build number.
 VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
+echo $VERSION_CODE
 plutil -replace CFBundleVersion -string "$VERSION_CODE" $(pwd)/ios/apolloschurchapp/Info.plist
+echo "IOS"
 sed -i "" 's/versionCode [^"]*/versionCode '$VERSION_CODE'/' $(pwd)/android/app/build.gradle
+echo "Android"
