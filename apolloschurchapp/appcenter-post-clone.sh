ee61abe23c131582030a29973eaa1065b904cf26
Vincent Wilson <vince@classyh.at>
2020-03-30 11:06:30 -0400

    Fix appcenter scripts

--- apolloschurchapp/appcenter-post-clone.sh
+++ apolloschurchapp/appcenter-post-clone.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-brew uninstall node@6
-NODE_VERSION="8.11.3"
-curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
-
-# Appcenter needs a yarn.lock file to exist next to the package.json to detect yarn:
-cp $(pwd)/../../yarn.lock $(pwd)/yarn.lock
 
 # Swaps out all placeholder env variables w/ their real values
 # Placeholders look like "$ONE_SIGNAL_KEY"
--- apolloschurchapp/appcenter-pre-build.sh
+++ apolloschurchapp/appcenter-pre-build.sh
@@ -7,6 +7,7 @@ cp $droid_dir/gradle.properties.appcenter $droid_dir/gradle.properties
 cd ios
 pod install
 
-cd ../../..
-yarn
-yarn lerna run generate-stories
+# You can comment this out if your app doesn't have an existing build number.
+VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
+plutil -replace CFBundleVersion -string "$VERSION_CODE" $(pwd)/ios/apolloschurchapp/Info.plist
+sed -i "" 's/versionCode [^"]*/versionCode '$VERSION_CODE'/' $(pwd)/android/app/build.gradle
