516b4e1002e9e3a604fe02f30ef0ca0feb6d1dce
Burke Shartsis <burke@differential.com>
2020-04-10 20:35:23 -0400

    update HorizontalCardListFeature import to use ui-connected

--- apolloschurchapp/src/tabs/home/Features/index.js
+++ apolloschurchapp/src/tabs/home/Features/index.js
@@ -3,9 +3,10 @@ import { Query } from 'react-apollo';
 import { get } from 'lodash';
 // import PropTypes from 'prop-types';
 
+import { HorizontalCardListFeature } from '@apollosproject/ui-connected';
+
 import ActionListFeature from './ActionListFeature';
 import CampaignItemListFeature from './CampaignItemListFeature';
-import HorizontalCardListFeature from './HorizontalCardListFeature';
 import VerticalCardListFeature from './VerticalCardListFeature';
 
 import GET_FEED_FEATURES from './getFeedFeatures';
