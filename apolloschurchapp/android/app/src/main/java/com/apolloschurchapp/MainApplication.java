0009222e8a7e5f9e0e1758e8f99cc5cc48152388
Vincent Wilson <vincent@differential.com>
2020-05-14 10:58:05 -0400

    Android building and running

--- apolloschurchapp/android/app/src/main/java/com/apolloschurchapp/MainApplication.java
+++ apolloschurchapp/android/app/src/main/java/com/apolloschurchapp/MainApplication.java
@@ -1,6 +1,6 @@
 package com.apolloschurchapp;
 
-import android.app.Application;
+import androidx.multidex.MultiDexApplication;
 import android.content.Context;
 
 import com.facebook.react.PackageList;
@@ -16,7 +16,7 @@ import java.util.Arrays;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
---
+++ apolloschurchapp/react-native-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dependencies: {
+    'react-native-passkit-wallet': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+      },
+    },
+  },
+};
