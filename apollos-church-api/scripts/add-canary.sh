5c46536f56ed1b20cbe0c06854721a76e7b15c2d
Michael Neeley <micneeley14@gmail.com>
2020-08-24 13:50:41 -0400

    Adds Canary Packages to Dev Process

---
+++ apollos-church-api/scripts/add-canary.sh
@@ -0,0 +1,21 @@
+# dependecies
+yarn add @apollosproject/config@canary
+yarn add @apollosproject/bugsnag@canary
+yarn add @apollosproject/data-connector-algolia-search@canary
+yarn add @apollosproject/data-connector-analytics@canary
+yarn add @apollosproject/data-connector-bible@canary
+yarn add @apollosproject/data-connector-church-online@canary
+yarn add @apollosproject/data-connector-cloudinary@canary
+yarn add @apollosproject/data-connector-onesignal@canary
+yarn add @apollosproject/data-connector-passes@canary
+yarn add @apollosproject/data-connector-redis-cache@canary
+yarn add @apollosproject/data-connector-rock@canary
+yarn add @apollosproject/data-connector-twilio@canary
+yarn add @apollosproject/data-schema@canary
+yarn add @apollosproject/rock-apollo-data-source@canary
+yarn add @apollosproject/server-core@canary
+
+# dev dependencies
+yarn add --dev @apollosproject/apollo-server-env-mock@canary
+yarn add --dev @apollosproject/babel-preset-apollos@canary
+yarn add --dev @apollosproject/eslint-config@canary
---
+++ apolloschurchapp/scripts/add-canary.sh
@@ -0,0 +1,20 @@
+# dependecies
+yarn add @apollosproject/config@canary
+yarn add @apollosproject/react-native-airplay-btn@canary
+yarn add @apollosproject/ui-analytics@canary
+yarn add @apollosproject/ui-auth@canary
+yarn add @apollosproject/ui-connected@canary
+yarn add @apollosproject/ui-fragments@canary
+yarn add @apollosproject/ui-htmlview@canary
+yarn add @apollosproject/ui-kit@canary
+yarn add @apollosproject/ui-mapview@canary
+yarn add @apollosproject/ui-media-player@canary
+yarn add @apollosproject/ui-notifications@canary
+yarn add @apollosproject/ui-onboarding@canary
+yarn add @apollosproject/ui-passes@canary
+yarn add @apollosproject/ui-prayer@canary
+yarn add @apollosproject/ui-scripture@canary
+yarn add @apollosproject/ui-storybook@canary
+
+# dev dependencies
+yarn add --dev @apollosproject/eslint-config@canary
