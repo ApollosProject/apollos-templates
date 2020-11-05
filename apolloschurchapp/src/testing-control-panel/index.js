67ebd05e4603d8a1141b933931b9e09c80421431
Michael Neeley <micneeley14@gmail.com>
2020-11-05 15:50:03 -0500

    removes utils analytics and test browser

--- apolloschurchapp/src/testing-control-panel/index.js
+++ apolloschurchapp/src/testing-control-panel/index.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import { TableView } from '@apollosproject/ui-kit';
-import { RockAuthedWebBrowser } from '@apollosproject/ui-connected';
 import TouchableCell from './TouchableCell';
 
 export default class TestingControlPanel extends PureComponent {
@@ -16,39 +15,6 @@ export default class TestingControlPanel extends PureComponent {
   render() {
     return (
       <TableView>
-        <RockAuthedWebBrowser>
-          {(openUrl) => (
-            <>
-              <TouchableCell
-                handlePress={() =>
-                  openUrl(
-                    'https://apollosrock.newspring.cc',
-                    {},
-                    { useRockToken: true }
-                  )
-                }
-                iconName="share"
-                cellText={`Open InAppBrowser With Rock Token`}
-              />
-              <TouchableCell
-                handlePress={() =>
-                  openUrl(
-                    'https://apollosrock.newspring.cc',
-                    { externalBrowser: true },
-                    { useRockToken: true }
-                  )
-                }
-                iconName="share"
-                cellText={`Open Safari With Rock Token`}
-              />
-              <TouchableCell
-                handlePress={() => openUrl('mailto:fake@apollosproject.com')}
-                iconName="share"
-                cellText={`Open Email link`}
-              />
-            </>
-          )}
-        </RockAuthedWebBrowser>
         <TouchableCell
           handlePress={() => this.props.navigation.navigate('Onboarding')}
           iconName="Avatar"
