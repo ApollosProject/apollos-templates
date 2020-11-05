a6670ce6e62dafc4d7419cba200db81a83745250
Michael Neeley <micneeley14@gmail.com>
2020-11-05 15:50:54 -0500

    removes utils tests

--- apolloschurchapp/src/utils/__tests__/utils.tests.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-import { renderWithApolloData } from '../testUtils';
-
-test('renderWithApolloData renders the component', async () => {
-  const tree = await renderWithApolloData(<Text>hello, world!</Text>);
-  expect(tree).toMatchSnapshot();
-});
