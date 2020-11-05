606a502992ae3583f464509ab7bc10a9c0a0b28b
Michael Neeley <micneeley14@gmail.com>
2020-11-05 15:27:28 -0500

    first commit

--- apolloschurchapp/src/index.js
+++ apolloschurchapp/src/index.js
@@ -23,7 +23,6 @@ import Tabs from './tabs';
 import PersonalDetails from './user-settings/PersonalDetails';
 import ChangePassword from './user-settings/ChangePassword';
 import LandingScreen from './LandingScreen';
-import UserWebBrowser from './user-web-browser';
 import Onboarding from './ui/Onboarding';
 
 const AppStatusBar = withTheme(({ theme }) => ({
@@ -54,7 +53,6 @@ const AppNavigator = createStackNavigator(
     ChangePassword,
     Location,
     Passes,
-    UserWebBrowser,
     Onboarding,
     LandingScreen,
   },
