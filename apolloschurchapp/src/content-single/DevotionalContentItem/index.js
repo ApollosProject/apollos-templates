af4af868379963f8a5f4bee6f148055a37c63233
Michael Neeley <micneeley14@gmail.com>
2020-11-25 11:02:20 -0500

    safe area view

--- apolloschurchapp/src/content-single/DevotionalContentItem/index.js
+++ apolloschurchapp/src/content-single/DevotionalContentItem/index.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Query } from '@apollo/client/react/components';
 import { get } from 'lodash';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import {
   ErrorCard,
