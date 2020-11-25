feb55af22add368f01f54dbad0ed569b4b31b425
Vincent Wilson <vincent@differential.com>
2020-11-25 10:00:57 -0500

    Fix onboarding tests

--- apolloschurchapp/src/ui/Onboarding/Onboarding.tests.js
+++ apolloschurchapp/src/ui/Onboarding/Onboarding.tests.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Providers, renderWithApolloData } from '@apollosproject/ui-test-utils';
+import { MockedProvider } from '@apollo/client/testing';
 
 import Onboarding from '.';
 
 describe('the Onboarding component', () => {
   it('should render Onboarding', async () => {
     const tree = await renderWithApolloData(
-      <Providers>
+      <Providers MockedProvider={MockedProvider}>
         <Onboarding />
       </Providers>
     );
