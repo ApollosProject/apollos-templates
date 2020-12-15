4663df025995ec9263e9aff740fc6273ed1da63c
Michael Neeley <micneeley14@gmail.com>
2020-12-15 11:45:42 -0500

    Moves Linking from Yalc to Yarn

--- apolloschurchapp/scripts/link-packages.js
+++ apolloschurchapp/scripts/link-packages.js
@@ -8,9 +8,14 @@ const packageJson = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`));
 const dependencies = [
   ...Object.keys(packageJson.dependencies),
   ...Object.keys(packageJson.devDependencies),
-].filter((d) => d.includes('@apollosproject'));
+].filter(
+  (d) =>
+    d.includes('@apollosproject') &&
+    !d.includes('react-native-airplay-btn') &&
+    !d.includes('storybook')
+);
 
 dependencies.forEach((dep) => {
-  const output = execSync(`yalc ${direction} ${dep}`, { encoding: 'utf8' });
+  const output = execSync(`yarn ${direction} ${dep}`, { encoding: 'utf8' });
   console.log(output);
 });
