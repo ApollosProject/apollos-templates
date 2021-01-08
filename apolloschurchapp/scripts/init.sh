aef5edf7d219d8030c2c2070e18de7870f8451a3
Michael Neeley <micneeley14@gmail.com>
2021-01-08 15:04:30 -0500

    removed appcenter files

--- apolloschurchapp/scripts/init.sh
+++ apolloschurchapp/scripts/init.sh
@@ -3,5 +3,4 @@ if test -f .env; then
 	echo ".env file already exists!"
 	exit 1
 fi
-echo "APP_DATA_URL=http://localhost:4000" > .env
-echo "APP_DATA_URL=${APP_DATA_URL}" > .env.production
+echo "APP_DATA_URL=http://localhost:4000" >.env
