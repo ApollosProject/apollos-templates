f25aa5995e80a2568de94b0a7ac7435ced16ea64
Michael Neeley <micneeley14@gmail.com>
2021-01-27 13:52:05 -0500

    Cleans up Android URL

--- apolloschurchapp/src/client/httpLink.js
+++ apolloschurchapp/src/client/httpLink.js
@@ -1,14 +1,9 @@
-import { Platform } from 'react-native';
 import { createUploadLink } from 'apollo-upload-client';
 import ApollosConfig from '@apollosproject/config';
 import { split, createHttpLink } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
 
-let uri = ApollosConfig.APP_DATA_URL;
-const androidUri = ApollosConfig.ANDROID_URL || '10.0.2.2';
-
-// Android's emulator requires localhost network traffic to go through 10.0.2.2
-if (Platform.OS === 'android') uri = uri.replace('localhost', androidUri);
+const uri = ApollosConfig.APP_DATA_URL;
 
 export default split(
   ({ query }) => {
