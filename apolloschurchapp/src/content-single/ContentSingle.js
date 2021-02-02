577904d18dcfe09fd2abb9f5ce05730c4388c7fd
Vincent Wilson <vince@classyh.at>
2021-02-02 06:50:30 -0800

    Update apolloschurchapp/src/content-single/ContentSingle.js

--- apolloschurchapp/src/content-single/ContentSingle.js
+++ apolloschurchapp/src/content-single/ContentSingle.js
@@ -21,7 +21,7 @@ const ContentSingle = (props) => {
   const nodeId = props.route?.params?.itemId;
   const { data, loading } = useQuery(
     gql`
-      query getContentNodeTitle($nodeId: ID) {
+      query getContentNodeTitle($nodeId: ID!) {
         node(id: $nodeId) {
           id
           ... on ContentNode {
