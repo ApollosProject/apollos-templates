e257004e2efff10b2dc5fe1c2d0b9900b6a87ee6
Michael Neeley <micneeley14@gmail.com>
2021-03-04 17:28:27 -0500

    add give content to action bar

--- apolloschurchapp/src/tabs/connect/ActionBar.js
+++ apolloschurchapp/src/tabs/connect/ActionBar.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { ActionBar, ActionBarItem } from '@apollosproject/ui-kit';
 import { useNavigation } from '@react-navigation/native';
-import { RockAuthedWebBrowser } from '@apollosproject/ui-connected';
 
 const Toolbar = () => {
   const navigation = useNavigation();
   return (
-    <RockAuthedWebBrowser>
-      {(openUrl) => (
-        <ActionBar>
-          <ActionBarItem
-            onPress={() => navigation.navigate('Passes')}
-            icon="check"
-            label="Check-in"
-          />
-          <ActionBarItem
-            onPress={() => openUrl('https://apollosrock.newspring.cc/page/186')}
-            icon="download"
-            label="Give"
-          />
-          <ActionBarItem
-            onPress={() => navigation.navigate('TestingControlPanel')}
-            icon="information"
-            label="Test"
-          />
-        </ActionBar>
-      )}
-    </RockAuthedWebBrowser>
+    <ActionBar>
+      <ActionBarItem
+        onPress={() => navigation.navigate('Passes')}
+        icon="check"
+        label="Check-in"
+      />
+      <ActionBarItem
+        onPress={() =>
+          navigation.navigate('NodeSingle', {
+            nodeId: 'UniversalContentItem:dcd4cbe22b380eed15f3e5c7604d09a6',
+          })
+        }
+        icon="download"
+        label="Give"
+      />
+      <ActionBarItem
+        onPress={() => navigation.navigate('TestingControlPanel')}
+        icon="information"
+        label="Test"
+      />
+    </ActionBar>
   );
 };
 
