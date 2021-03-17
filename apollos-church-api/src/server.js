a721d2534f1dd6a2ae27f458d6453731d38555f7
Michael Neeley <micneeley14@gmail.com>
2021-03-17 13:02:38 -0400

    fixed linter

--- apollos-church-api/src/server.js
+++ apollos-church-api/src/server.js
@@ -8,7 +8,10 @@ import { RockLoggingExtension } from '@apollosproject/rock-apollo-data-source';
 import { get } from 'lodash';
 import { setupUniversalLinks } from '@apollosproject/server-core';
 import { BugsnagPlugin } from '@apollosproject/bugsnag';
-import { sync, createMigrationRunner } from '@apollosproject/data-connector-postgres';
+import {
+  sync,
+  createMigrationRunner,
+} from '@apollosproject/data-connector-postgres';
 
 let dataObj;
 
