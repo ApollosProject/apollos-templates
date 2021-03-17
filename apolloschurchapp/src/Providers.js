0fa29c075a3bc417fbe48f184749d5134fa15fb8
Vincent Wilson <vincent@differential.com>
2021-03-17 16:28:51 -0400

    Integrated with notification action

--- apolloschurchapp/src/Providers.js
+++ apolloschurchapp/src/Providers.js
@@ -4,7 +4,10 @@ import { Providers, NavigationService } from '@apollosproject/ui-kit';
 import { AuthProvider } from '@apollosproject/ui-auth';
 import { AnalyticsProvider } from '@apollosproject/ui-analytics';
 import { NotificationsProvider } from '@apollosproject/ui-notifications';
-import { LiveProvider } from '@apollosproject/ui-connected';
+import {
+  LiveProvider,
+  ACCEPT_FOLLOW_REQUEST,
+} from '@apollosproject/ui-connected';
 import { checkOnboardingStatusAndNavigate } from '@apollosproject/ui-onboarding';
 
 import ClientProvider, { client } from './client';
@@ -15,6 +18,14 @@ const AppProviders = (props) => (
     <NotificationsProvider
       oneSignalKey={ApollosConfig.ONE_SIGNAL_APP_ID}
       navigate={NavigationService.navigate}
+      actionMap={{
+        // accept a follow request when someone taps "accept" in a follow request push notification
+        acceptFollowRequest: ({ requestPersonId }) =>
+          client.mutate({
+            mutation: ACCEPT_FOLLOW_REQUEST,
+            variables: { personId: requestPersonId },
+          }),
+      }}
     >
       <AuthProvider
         navigateToAuth={() => NavigationService.navigate('Auth')}
