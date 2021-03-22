ce537e9a0bad2f10f1a3b7ca7c4172783aa4c398
Vincent Wilson <vincent@differential.com>
2021-03-22 14:26:28 -0400

    Update snaps

--- apolloschurchapp/jest.setup.js
+++ apolloschurchapp/jest.setup.js
@@ -24,3 +24,20 @@ Animated.spring = (value, config) => ({
 });
 
 jest.mock('./src/client/index');
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  const Easing = {
+    exp: jest.fn(),
+    out: jest.fn(),
+  };
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+  Reanimated.Easing = Easing;
+  Reanimated.EasingNode = Easing;
+
+  return Reanimated;
+});
