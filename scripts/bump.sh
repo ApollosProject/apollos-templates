622e837a91767dbdbd50c2eb94f5ae55d6693c55
Michael Neeley <micneeley14@gmail.com>
2021-03-23 10:10:05 -0400

    podfile lock

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -22,6 +22,7 @@ if [[ "$VERSION" != "$PKG" ]]; then
 fi
 
 ./scripts/add-packages.sh latest
+yarn # this is to update Pods
 (cd apollos-church-api && sed -i "" -E "s/\"[0-9].*\"/\"$VERSION\"/g" apollos.json)
 (cd apolloschurchapp && sed -i "" -E "s/\"[0-9].*\"/\"$VERSION\"/g" apollos.json)
 
