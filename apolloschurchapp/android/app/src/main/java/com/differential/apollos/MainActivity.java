c93a9febfd88f3e9695426419180ac7a8b22bb06
Michael Neeley <micneeley14@gmail.com>
2021-03-24 16:26:39 -0400

    remove Google Cast
    Removes all the infrastructure required for Google cast, there was still surprisingly a lot. This gives us back a couple of permissions and maybe will other things smoother. Fingers crossed, this fixes being able to put release builds on a sim, the error currently is with Google Cast.

--- apolloschurchapp/android/app/src/main/java/com/differential/apollos/MainActivity.java
+++ apolloschurchapp/android/app/src/main/java/com/differential/apollos/MainActivity.java
@@ -5,9 +5,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import com.facebook.react.GoogleCastActivity;
 
-public class MainActivity extends GoogleCastActivity {
+public class MainActivity extends ReactActivity {
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
