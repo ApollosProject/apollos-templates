af5f5f99076e91603df9517f2289b5fde78f52f6
Michael Neeley <micneeley14@gmail.com>
2021-05-03 16:29:59 -0400

    initialization improvements
    This attempts to make the setup process smoother in various ways

--- apollos-church-api/scripts/init.sh
+++ apollos-church-api/scripts/init.sh
@@ -3,5 +3,24 @@ if test -f .env; then
 	echo ".env file already exists!"
 	exit 1
 fi
-echo "ROCK_API=
-ROCK_TOKEN=" > .env
+
+echo "You will need the following before beginning:
+Rock URL
+Rock API Token
+
+Are you ready to proceed? [y/n]: "
+read -r ANSWER
+if $ANSWER != "y"; then
+	exit 1
+fi
+
+echo "Rock URL: "
+read -r ROCK_URL
+echo "Rock API Token: "
+read -r ROCK_TOKEN
+
+echo "ROCK_URL=$ROCK_URL
+ROCK_TOKEN=$ROCK_TOKEN" >.env
+
+# remove template encrypted files
+rm .env.shared.enc
