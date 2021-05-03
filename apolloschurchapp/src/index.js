635e684b79bdc1e73d9229c83142a2d4feb8a6df
Michael Neeley <micneeley14@gmail.com>
2021-05-03 13:24:17 -0400

    provides endpoint to reset password and method in the app (#267)

--- apolloschurchapp/src/index.js
+++ apolloschurchapp/src/index.js
@@ -17,6 +17,7 @@ import {
 import Passes from '@apollosproject/ui-passes';
 import { MapViewConnected as Location } from '@apollosproject/ui-mapview';
 import Auth, { ProtectedRoute } from '@apollosproject/ui-auth';
+import ApollosConfig from '@apollosproject/config';
 
 import Providers from './Providers';
 import ContentSingle from './content-single';
@@ -39,8 +40,16 @@ const ProtectedRouteWithSplashScreen = (props) => {
   return <ProtectedRoute {...props} onRouteChange={handleOnRouteChange} />;
 };
 
+const { APP_DATA_URL } = ApollosConfig;
+
 // Hack to avoid needing to pass emailRequired through the navigator.navigate
-const EnhancedAuth = (props) => <Auth {...props} emailRequired />;
+const EnhancedAuth = (props) => (
+  <Auth
+    {...props}
+    emailRequired
+    forgotPasswordURL={`${APP_DATA_URL}/forgot-password`}
+  />
+);
 // 😑
 hoistNonReactStatic(EnhancedAuth, Auth);
 
