9426a528ef0e6f738447fad255706490f4dc096a
Michael Neeley <micneeley14@gmail.com>
2021-05-04 09:54:23 -0400

    autodetect apollos plugin

--- apollos-church-api/src/config.js
+++ apollos-church-api/src/config.js
@@ -1,7 +1,19 @@
 import path from 'path';
-import dotenv from 'dotenv/config'; // eslint-disable-line
+import fetch from 'node-fetch';
+import dotenv from "dotenv/config"; // eslint-disable-line
 import ApollosConfig from '@apollosproject/config';
 
 ApollosConfig.loadYaml({
   configPath: path.join(__dirname, '..', 'config.yml'),
 });
+
+// autodetect Apollos plugin
+(async () => {
+  if (!ApollosConfig.ROCK) return;
+  const res = await fetch(
+    `${ApollosConfig.ROCK.URL}/api/Apollos/GetContentChannelItemsByIds?ids=0`
+  );
+  const usePlugin = res.status === 200;
+  if (usePlugin) console.log('Apollos Rock plugin detected!');
+  ApollosConfig.loadJs({ ROCK: { USE_PLUGIN: usePlugin } });
+})();
