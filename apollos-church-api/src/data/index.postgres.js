5b0daab517d4e82b771256e8075e116f99231195
Vincent Wilson <vince@classyh.at>
2021-06-07 15:16:29 -0400

    Fix data connector ordering.

--- apollos-church-api/src/data/index.postgres.js
+++ apollos-church-api/src/data/index.postgres.js
@@ -92,8 +92,8 @@ const data = {
   Persona,
   Notification,
   NotificationPreference,
+  OneSignal,  
   Person, // An extension of Postgres person. Will be eliminated in the near future so you can use just postgres/Person.
-  OneSignal,
 };
 
 const {
