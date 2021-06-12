52a6cbf907263da8cd38210bc2b00b6992629df1
Michael Neeley <micneeley14@gmail.com>
2021-06-11 22:51:03 -0400

    move testing panel to user settings
    also deletes a lot of code and hides testing panel behind development environment so we don't have to worry about changing anything for new app spin ups

--- apolloschurchapp/src/user-settings/index.js
+++ apolloschurchapp/src/user-settings/index.js
@@ -1,7 +1,7 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { ScrollView } from 'react-native';
-import PropTypes from 'prop-types';
-import { Query, Mutation } from '@apollo/client/react/components';
+import { useQuery, useMutation, useApolloClient, gql } from '@apollo/client';
+import { useNavigation } from '@react-navigation/native';
 import { getVersion, getBuildNumber } from 'react-native-device-info';
 
 import {
@@ -14,150 +14,198 @@ import {
   Touchable,
   ActivityIndicator,
   NavigationService,
+  H4,
+  PaddedView,
 } from '@apollosproject/ui-kit';
-
+import {
+  checkOnboardingStatusAndNavigate,
+  onboardingComplete,
+} from '@apollosproject/ui-onboarding';
 import { GET_LOGIN_STATE, LOGOUT } from '@apollosproject/ui-auth';
 import {
   RockAuthedWebBrowser,
   UserAvatarUpdate,
 } from '@apollosproject/ui-connected';
 
-class UserSettings extends PureComponent {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func,
-    }),
-  };
+const UserSettings = () => {
+  const navigation = useNavigation();
+  const {
+    data: { isLoggedIn = false },
+    loading,
+  } = useQuery(GET_LOGIN_STATE, { fetchPolicy: 'cache-and-network' });
+  const [logout] = useMutation(LOGOUT);
+  const client = useApolloClient();
+  const { data } = useQuery(gql`
+    query currentUserId {
+      currentUser {
+        id
+      }
+    }
+  `);
 
-  render() {
-    return (
-      <Query query={GET_LOGIN_STATE} fetchPolicy="cache-and-network">
-        {({ data: { isLoggedIn = false }, loading }) => {
-          if (loading) return <ActivityIndicator />;
-          if (!isLoggedIn) return null;
-          return (
-            <BackgroundView>
-              <ScrollView>
-                <UserAvatarUpdate />
+  if (loading) return <ActivityIndicator />;
+  if (!isLoggedIn) return null;
+  return (
+    <BackgroundView>
+      <ScrollView>
+        <UserAvatarUpdate />
 
-                <RockAuthedWebBrowser>
-                  {(openUrl) => (
-                    <>
-                      <TableView>
-                        <Touchable
-                          onPress={() => {
-                            this.props.navigation.navigate('PersonalDetails');
-                          }}
-                        >
-                          <Cell>
-                            <CellText>Personal Details</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                        <Divider />
-                        <Touchable
-                          onPress={() => {
-                            this.props.navigation.navigate('Location');
-                          }}
-                        >
-                          <Cell>
-                            <CellText>Location</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                        <Divider />
-                        <Touchable
-                          onPress={() => {
-                            this.props.navigation.navigate('ChangePassword');
-                          }}
-                        >
-                          <Cell>
-                            <CellText>Change Password</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                        <Touchable
-                          onPress={() => {
-                            this.props.navigation.navigate('Notifications');
-                          }}
-                        >
-                          <Cell>
-                            <CellText>Notification Settings</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                      </TableView>
-                      <TableView>
-                        <Touchable
-                          onPress={() => {
-                            openUrl('mailto:support@apollos.app');
-                          }}
-                        >
-                          <Cell>
-                            <CellText>Give Feedback</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                      </TableView>
-                      <TableView>
-                        <Touchable
-                          onPress={() =>
-                            openUrl('https://apollosrock.newspring.cc/')
-                          }
-                        >
-                          <Cell>
-                            <CellText>Privacy Policy</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                        <Divider />
-                        <Touchable
-                          onPress={() =>
-                            openUrl('https://apollosrock.newspring.cc/')
-                          }
-                        >
-                          <Cell>
-                            <CellText>Terms of Use</CellText>
-                            <CellIcon name="arrow-next" />
-                          </Cell>
-                        </Touchable>
-                      </TableView>
-                      <TableView>
-                        <Mutation mutation={LOGOUT}>
-                          {(handleLogout) => (
-                            <Touchable
-                              onPress={async () => {
-                                await handleLogout();
-                                // This resets the navigation stack, and the navigates to the first auth screen.
-                                // This ensures that user isn't navigated to a subscreen of Auth, like the pin entry screen.
-                                await NavigationService.resetToAuth();
-                              }}
-                            >
-                              <Cell>
-                                <CellText>Logout</CellText>
-                                <CellIcon name="arrow-next" />
-                              </Cell>
-                            </Touchable>
-                          )}
-                        </Mutation>
-                      </TableView>
-                      <TableView>
-                        <Cell>
-                          <CellText>
-                            {`App Version: ${getVersion()}.${getBuildNumber()}`}
-                          </CellText>
-                        </Cell>
-                      </TableView>
-                    </>
-                  )}
-                </RockAuthedWebBrowser>
-              </ScrollView>
-            </BackgroundView>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+        <RockAuthedWebBrowser>
+          {(openUrl) => (
+            <>
+              <TableView>
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate('PersonalDetails');
+                  }}
+                >
+                  <Cell>
+                    <CellText>Personal Details</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+                <Divider />
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate('Location');
+                  }}
+                >
+                  <Cell>
+                    <CellText>Location</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+                <Divider />
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate('ChangePassword');
+                  }}
+                >
+                  <Cell>
+                    <CellText>Change Password</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate('Notifications');
+                  }}
+                >
+                  <Cell>
+                    <CellText>Notification Settings</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+              </TableView>
+              <TableView>
+                <Touchable
+                  onPress={() => {
+                    openUrl('mailto:support@apollos.app');
+                  }}
+                >
+                  <Cell>
+                    <CellText>Give Feedback</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+              </TableView>
+              <TableView>
+                <Touchable
+                  onPress={() => openUrl('https://apollosrock.newspring.cc/')}
+                >
+                  <Cell>
+                    <CellText>Privacy Policy</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+                <Divider />
+                <Touchable
+                  onPress={() => openUrl('https://apollosrock.newspring.cc/')}
+                >
+                  <Cell>
+                    <CellText>Terms of Use</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+              </TableView>
+              <TableView>
+                <Touchable
+                  onPress={async () => {
+                    await logout();
+                    // This resets the navigation stack, and the navigates to the first auth screen.
+                    // This ensures that user isn't navigated to a subscreen of Auth, like the pin entry screen.
+                    await NavigationService.resetToAuth();
+                  }}
+                >
+                  <Cell>
+                    <CellText>Logout</CellText>
+                    <CellIcon name="arrow-next" />
+                  </Cell>
+                </Touchable>
+              </TableView>
+              <TableView>
+                <Cell>
+                  <CellText>
+                    {`App Version: ${getVersion()}.${getBuildNumber()}`}
+                  </CellText>
+                </Cell>
+              </TableView>
+
+              {/* testing panel */}
+              {process.env.NODE_ENV !== 'production' ? (
+                <>
+                  <PaddedView>
+                    <H4>For development only</H4>
+                  </PaddedView>
+                  <TableView>
+                    <Touchable
+                      onPress={() =>
+                        checkOnboardingStatusAndNavigate({
+                          latestOnboardingVersion: 2,
+                          navigation: NavigationService,
+                          client,
+                        })
+                      }
+                    >
+                      <Cell>
+                        <CellText>Launch Onboarding</CellText>
+                      </Cell>
+                    </Touchable>
+                    <Divider />
+                    <Touchable
+                      onPress={() =>
+                        onboardingComplete({
+                          version: 0,
+                          userId: data?.currentUser?.id,
+                        })
+                      }
+                    >
+                      <Cell>
+                        <CellText>Reset Onboarding to Unseen</CellText>
+                      </Cell>
+                    </Touchable>
+                    <Divider />
+                    <Touchable
+                      onPress={() =>
+                        onboardingComplete({
+                          version: 1,
+                          userId: data?.currentUser?.id,
+                        })
+                      }
+                    >
+                      <Cell>
+                        <CellText>Reset Onboarding to Seen v1</CellText>
+                      </Cell>
+                    </Touchable>
+                  </TableView>
+                </>
+              ) : null}
+            </>
+          )}
+        </RockAuthedWebBrowser>
+      </ScrollView>
+    </BackgroundView>
+  );
+};
 
 export default UserSettings;
