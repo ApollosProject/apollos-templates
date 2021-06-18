6d72fb9174030234acc313597cf4c3216dec975e
Vincent Wilson <vince@classyh.at>
2021-06-18 12:30:40 -0400

    Move providers inside nav stack (#404)
    * Move providers inside nav stack
    
    * Add prop types dec
    
    * podfile lock
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

--- apolloschurchapp/src/Providers.js
+++ apolloschurchapp/src/Providers.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ApollosConfig from '@apollosproject/config';
-import { Providers, NavigationService } from '@apollosproject/ui-kit';
+import { NavigationService } from '@apollosproject/ui-kit';
 import { AuthProvider } from '@apollosproject/ui-auth';
 import { AnalyticsProvider } from '@apollosproject/ui-analytics';
 import { NotificationsProvider } from '@apollosproject/ui-notifications';
@@ -11,7 +12,6 @@ import {
 import { checkOnboardingStatusAndNavigate } from '@apollosproject/ui-onboarding';
 
 import ClientProvider, { client } from './client';
-import customTheme, { customIcons } from './theme';
 
 const AppProviders = (props) => (
   <ClientProvider {...props}>
@@ -50,17 +50,15 @@ const AppProviders = (props) => (
         }
       >
         <AnalyticsProvider>
-          <LiveProvider>
-            <Providers
-              themeInput={customTheme}
-              iconInput={customIcons}
-              {...props}
-            />
-          </LiveProvider>
+          <LiveProvider>{props.children}</LiveProvider>
         </AnalyticsProvider>
       </AuthProvider>
     </NotificationsProvider>
   </ClientProvider>
 );
+
+AppProviders.propTypes = {
+  children: PropTypes.shape({}),
+};
 
 export default AppProviders;
