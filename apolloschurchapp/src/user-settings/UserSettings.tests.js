3b70850e79c7f0a4de16e3f2e8145466224a69c8
Michael Neeley <micneeley14@gmail.com>
2021-06-25 15:20:34 -0400

    move testing panel to user settings (#389)
    * move testing panel to user settings
    
    also deletes a lot of code and hides testing panel behind development environment so we don't have to worry about changing anything for new app spin ups
    
    * snap

--- apolloschurchapp/src/user-settings/UserSettings.tests.js
+++ apolloschurchapp/src/user-settings/UserSettings.tests.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 import { GET_LOGIN_STATE } from '@apollosproject/ui-auth';
-import { renderWithApolloData, Providers } from '@apollosproject/ui-test-utils';
+import {
+  renderWithApolloData,
+  Providers,
+  WithReactNavigator,
+} from '@apollosproject/ui-test-utils';
+import { gql } from '@apollo/client';
 import { MockedProvider } from '@apollo/client/testing';
 import { InMemoryCache } from '@apollo/client/cache';
 
 import UserSettings from '.';
+
+const mock = {
+  request: {
+    query: gql`
+      query currentUserId {
+        currentUser {
+          id
+        }
+      }
+    `,
+  },
+  result: {
+    data: {
+      currentUser: {
+        id: 1,
+      },
+    },
+  },
+};
 
 describe('UserSettings component', () => {
   it('renders UserSettings when logged in', async () => {
@@ -14,11 +38,12 @@ describe('UserSettings component', () => {
       query: GET_LOGIN_STATE,
       data: { isLoggedIn: true },
     });
-    const navigation = { navigate: jest.fn() };
     const tree = await renderWithApolloData(
-      <Providers cache={cache} MockedProvider={MockedProvider}>
-        <UserSettings navigation={navigation} />
-      </Providers>
+      WithReactNavigator(
+        <Providers cache={cache} MockedProvider={MockedProvider} mocks={[mock]}>
+          <UserSettings />
+        </Providers>
+      )
     );
     expect(tree).toMatchSnapshot();
   });
