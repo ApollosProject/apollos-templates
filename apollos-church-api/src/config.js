b5b7cd022dcca9ea8e810d5e172e79d25f0f0b64
Michael Neeley <micneeley14@gmail.com>
2021-07-16 21:23:41 -0400

    replaces config variables with env variables

--- apollos-church-api/src/config.js
+++ apollos-church-api/src/config.js
@@ -3,13 +3,9 @@ import fetch from 'node-fetch';
 import dotenv from "dotenv/config"; // eslint-disable-line
 import ApollosConfig from '@apollosproject/config';
 
+// load in tab features
 ApollosConfig.loadYaml({
   configPath: path.join(__dirname, '..', 'config.yml'),
-});
-
-// defaults
-ApollosConfig.loadJs({
-  ROCK: { TIMEZONE: ApollosConfig?.ROCK?.TIMEZONE || 'America/New_York' },
 });
 
 // autodetect some settings
