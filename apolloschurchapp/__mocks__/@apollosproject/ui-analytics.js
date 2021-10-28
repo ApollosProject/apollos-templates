6d1da77e255d1ad3c48bd32170f768df6e12ea82
Michael Neeley <micneeley14@gmail.com>
2021-10-28 16:50:14 -0400

    removes mocks

--- apolloschurchapp/__mocks__/@apollosproject/ui-analytics.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  track: () => '',
-  AnalyticsConsumer: ({ children }) => children({ test: jest.fn() }),
-  AnalyticsProvider: ({ children }) => children,
-  CoreNavigationAnalytics: ({ children }) =>
-    children({ onNavigationStateChange: jest.fn() }),
-  TrackEventWhenLoaded: () => null,
-  withTrackOnPress: (Component) => (props) => <Component {...props} />,
-};
